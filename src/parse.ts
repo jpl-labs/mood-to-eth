4acd9350fcd9f33f0cc4957a259a9cbba5e1cef5
Kevin Brey <manbearwiz@gmail.com>
2017-07-19 14:08:33 -0500

    fix(web3): unlock account before sending transactions

--- src/parse.ts
+++ src/parse.ts
@@ -1,24 +1,25 @@
 import { PlayerStatus, AudioSong } from "./models/PlayerStatus";
 import { MoodPlayer } from "./services/MoodPlayer";
 
-import * as Web3 from "web3";
+import { Environment } from "./environment";
+
+const Web3 = require("web3");
 
 import * as wagerArtifacts from "../node_modules/tc2017-contract-artifacts/Wager.json";
 
-import { Contract, Network } from "tc2017-contract-artifacts"
+// import { Contract, Network } from "tc2017-contract-artifacts";
 
-const wagerContract : Contract = wagerArtifacts as any;
+// const wagerContract: Contract = wagerArtifacts as any;
 
 const contract = require("truffle-contract");
 
-const mood_uri = "https://192.168.0.100";
+const moodPlayer = new MoodPlayer(Environment.moodUri);
 
-const moodPlayer = new MoodPlayer(mood_uri);
-
-const thatConfProvider = new Web3.providers.HttpProvider("http://bcjl3x55m.eastus.cloudapp.azure.com:8545");
+const thatConfProvider = new Web3.providers.HttpProvider(Environment.web3Provider);
 
 console.log("Connecting to blockchain...");
-const web3 = new Web3();
+// const web3 = new Web3();
+const web3 = new Web3(thatConfProvider);
 web3.setProvider(thatConfProvider);
 const balance = web3.eth.getBalance(web3.eth.coinbase);
 console.log(balance.toString());
@@ -29,12 +30,12 @@ Wager.setProvider(thatConfProvider);
 
 moodPlayer.onSongChange().subscribe(song => {
     console.log(song);
-    //console.log(web3.toHex(JSON.stringify(song)));
     Wager.deployed()
         .then((instance: any) => {
-            instance.setWinningArtist(web3.toHex(song.artist), web3.toHex(JSON.stringify(song)), {
-                from: "0x72e98c3c1be92b3195fa3a6dc62ca90e77e6f9be",
-                gas: 1000000
+            web3.personal.unlockAccount(Environment.genesisAddress, Environment.genesisPassword, 2);
+            instance.endRound(web3.toHex(song.artist), web3.toHex(JSON.stringify(song)), {
+                from: Environment.genesisAddress,
+                gas: 4712388
             });
         });
 
--- src/services/MoodPlayer.ts
+++ src/services/MoodPlayer.ts
@@ -1,36 +1,38 @@
 import { Observable } from "rxjs";
-import { RxHR, RxHttpRequestResponse } from "@akanass/rx-http-request";
+import { RxHR, RxHttpRequestResponse, RxCookieJar } from "@akanass/rx-http-request";
 import "rxjs/add/operator/mergeMap";
 
 import { PlayerStatus, AudioSong } from "../models/PlayerStatus";
 
 export class MoodPlayer {
 
+    readonly cookieJar: Observable<RxCookieJar>;
 
-    constructor(private uri: string) {
+    constructor(private uri: string, private pollInterval: number = 1000) {
+        this.cookieJar = RxHR.jar();
     }
 
-    public onSongChange(): Observable<AudioSong> {
-        const j = RxHR.jar();
-
-        return RxHR.post(`${this.uri}/login`, {
+    public onSongChange = (): Observable<AudioSong> =>
+        RxHR.post(`${this.uri}/login`, {
             form: { user: "admin", password: "23646" },
             strictSSL: false,
-            jar: j
+            jar: this.cookieJar
         }).mergeMap(
             (response: RxHttpRequestResponse): Observable<AudioSong> =>
                 Observable
-                    .interval(1000)
-                    .switchMap(() => RxHR.post(`${this.uri}/cmd?cmd=zone.getStatus`, {
-                        form: { zoneId: 1 },
-                        strictSSL: false,
-                        jar: j,
-                        json: true
-                    }))
-                    .map((response: RxHttpRequestResponse): PlayerStatus => response.body)
+                    .interval(this.pollInterval)
+                    .switchMap(this.getStatus)
                     .map((playerStatus: PlayerStatus): AudioSong => playerStatus.data.currentAudioSong)
+                    .filter((song: AudioSong): boolean => !!song.artist)
                     .distinctUntilKeyChanged("id")
             );
-    }
+
+    public getStatus = (): Observable<PlayerStatus> =>
+        RxHR.post(`${this.uri}/cmd?cmd=zone.getStatus`, {
+            form: { zoneId: 1 },
+            strictSSL: false,
+            jar: this.cookieJar,
+            json: true
+        }).map((response: RxHttpRequestResponse): PlayerStatus => response.body);
 
 }
